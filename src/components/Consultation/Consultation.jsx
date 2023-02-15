2c144ffe97e8e8ab4a3bd59cee80bf13e8aa94dc
Your Name <you@example.com>
2023-02-16 02:07:06 +0300

    consultation

--- src/components/Consultation/Consultation.jsx
+++ src/components/Consultation/Consultation.jsx
@@ -3,12 +3,18 @@ import styles from './Consultation.module.scss'
 import Container from "@/Layout/Container/Container";
 import BtnWhite from "@/UI/BtnWhite/BtnWhite";
 import Modal from "@/UI/Modal/Modal";
+import bg from "../../../public/question.png";
 
 const Consultation = () => {
     const [openModal, setOpenModal] = useState(false )
 
     return (
-        <section className={styles.consultation}>
+        <section className={styles.consultation} style={{
+            backgroundImage: `url(${bg.src})`,
+            backgroundRepeat: 'no-repeat'
+            // width: '100%',
+            // height: '100%',>
+        }}>
             <Container>
                 <div className={styles.consultation_content}>
                     <div className={styles.info}>
