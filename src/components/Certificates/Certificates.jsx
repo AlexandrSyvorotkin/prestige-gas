dc1c005abe92250d7483421fb75723abb1d2cef9
Your Name <you@example.com>
2023-02-15 23:59:53 +0300

    certifitaces

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Certificates.module.scss'
 import Container from "@/Layout/Container/Container";
+import Image from "next/image";
+
+const certificates = [
+    {id:1 , path: '/cercificates/75.jpeg'},
+    {id:2 , path: '/cercificates/76.jpeg'},
+]
 
 const Certificates = () => {
     return (
@@ -9,6 +15,16 @@ const Certificates = () => {
                 <div className={styles.certificates_content}>
                     <div className={styles.header}>
                         Наши сертификаты
+                    </div>
+                    <div className={styles.info}>
+                        <span>Мастера рерулярно проходят обучение у официальных дилеров и поддерживают свои знания на высоком уровне. Чему свидетельствуют сертификаты ведущих производителей.</span>
+                        <div className={styles.list_items}>
+                            {certificates.map(it =>
+                                <div key={it.id} className={styles.single_certificate}>
+                                    <Image src={it.path} width='300' height='400'/>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </Container>
